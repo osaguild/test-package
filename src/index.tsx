63b39b4fcd1bff75b68c470b82c19e9869f73759
osaguild <nobuhisa.abe.0922@gmail.com>
2022-08-26 18:19:40 +0900

    fix for v0.2.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { Upload } from "@osaguild/ipfs-uploader";
+import { IpfsUploader } from "@osaguild/ipfs-uploader";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <StrictMode>
-    <Upload message="hello uploader" />
+    <IpfsUploader />
   </StrictMode>
 );
