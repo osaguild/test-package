12a39566330eb4d0ba2992f2bcbd143e134fa80d
osaguild <nobuhisa.abe.0922@gmail.com>
2022-08-26 23:06:41 +0900

    fix package

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { IpfsUploader } from "@osaguild/ipfs-uploader";
 
+const REACT_APP_PINATA_API_JWT = process.env.REACT_APP_PINATA_API_JWT as string;
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
   <StrictMode>
-    <IpfsUploader />
+    <IpfsUploader
+      pinataApiJwt={REACT_APP_PINATA_API_JWT}
+      callback={(event) => {
+        console.log(event.eventType);
+      }}
+    />
   </StrictMode>
 );
